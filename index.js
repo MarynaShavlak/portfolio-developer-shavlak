e9f47c8ebdf399cd1f863b09027af9dd252bc2e1
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-19 20:28:29 +0300

    add code for html refactoring

--- index.js
+++ index.js
@@ -3,13 +3,24 @@ import { initCVModal } from "./js/cvModal.js";
 import {initBlog} from "./js/blog.js";
 import {initReviewsSlider} from "./js/reviewSlider.js";
 import {initPortfolio} from "./js/portfolio.js";
-import {loadPartial} from "./js/partials-loader.js";
+import {insertPartial} from "./js/partials-loader.js";
 
-$(document).ready(function () {
-  loadPartial('header', '../partials/header.html');
-  initPortfolio();
-  initReviewsSlider();
-  initBlog();
-  initHireModal();
-  initCVModal();
+$(document).ready(async function () {
+  try {
+
+    await insertPartial('#header', '../partials/header.html');
+    await insertPartial('#main', '../partials/hero.html');
+    await insertPartial('#main', '../partials/portfolio.html');
+    await insertPartial('#main', '../partials/about.html');
+    await insertPartial('#main', '../partials/reviews.html');
+    await insertPartial('#main', '../partials/blog.html');
+
+    initPortfolio();
+    initReviewsSlider();
+    initBlog();
+    initHireModal();
+    initCVModal();
+  } catch (err) {
+    console.error('Error loading one or more partials:', err);
+  }
 });
--- js/partials-loader.js
+++ js/partials-loader.js
@@ -1,10 +1,27 @@
-export function loadPartial(id, url) {
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById(id).innerHTML = data;
-        })
-        .catch(error => console.error('Error loading partial:', url, error));
+export function insertPartial(selector, url, position = 'beforeend') {
+    return new Promise((resolve, reject) => {
+        const target = document.querySelector(selector);
+        if (!target) {
+            reject(`Target "${selector}" not found.`);
+            return;
+        }
+
+        fetch(url)
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error(`Failed to load ${url}`);
+                }
+                return res.text();
+            })
+            .then(html => {
+                target.insertAdjacentHTML(position, html);
+                resolve(); // Resolve when the HTML is inserted
+            })
+            .catch(err => {
+                console.error(`Error inserting partial:`, err);
+                reject(err); // Reject in case of error
+            });
+    });
 }
 
 
