13b4c9d7015c3bb4f787e4866d3104c3fb23b256
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-23 19:53:25 +0300

    add webstudio code

--- data/projectsData.js
+++ data/projectsData.js
@@ -171,7 +171,7 @@ export const projectsData = [
         title: 'Comfort Group Cleaning',
         slogan: 'Your Convenient Cleaning Solution!',
         date: 2023,
-        poster: 'poster.jpg',
+        poster: 'poster.gif',
         timeline: '3 months',
         website: {
             name: 'Comfort Group WebSite',
@@ -237,7 +237,7 @@ export const projectsData = [
             },
         ],
         techStack: {
-            architecture: 'Multi-page website',
+            architecture: 'Multi-page application (MPA) using Vanilla JavaScript',
             frontend: ['HTML/CSS/JS', 'SASS/SCSS', 'jQuery'],
             build: ['Gulp', 'Webpack', 'Babel'],
             tools: ['PostCSS', 'Autoprefixer'],
@@ -276,7 +276,7 @@ export const projectsData = [
             link: 'https://www.canva.com/design/DAFo_yYBwKY/2VVBeMIKfRF6uEI7AnxDvQ/edit'
         },
         date: 2023,
-        poster: 'poster.jpg',
+        poster: 'poster.gif',
         timeline: '2 months',
         website: {
             name: 'Sudoku for Kids WebGame',
@@ -790,7 +790,7 @@ export const projectsData = [
             },
         ],
         techStack: {
-            architecture: 'Multi-page Vanilla JS app with Parcel',
+            architecture: 'Multi-page application (MPA) using Vanilla JavaScript with Parcel',
             frontend: ['Vanilla JavaScript', 'HTML5', 'SCSS'],
             stateManagement: ['LocalStorage (for persistence)'],
             api: ['Axios', 'The Movie Database (TMDb) API'],
@@ -1033,6 +1033,101 @@ export const projectsData = [
             frontend: ['HTML5', 'SCSS', 'Vanilla JavaScript', 'Font Awesome' ],
             ui: ['Font Awesome icons'],
             tools: ['Prettier'],
+
+        },
+        images: [
+            'demo-video.mp4',
+            'img1.jpg',
+            'img2.jpg',
+            'img3.jpg',
+            'img4.jpg',
+            'img5.jpg',
+
+        ],
+    },
+    {
+        id: 'webStudio',
+        industry: 'Web Application',
+        categories: ['html', 'saas', 'javascript', ],
+        title: 'Web Studio Landing Page',
+        type: 'Individual',
+        slogan: 'Crafting Your Digital Universe!',
+        date: 2022,
+        poster: 'poster.jpg',
+        timeline: '1 week',
+
+        website: {
+            name: 'Web Studio Landing',
+            link: 'https://marynashavlak.github.io/web-studio/index.html',
+        },
+        repository: {
+            name:'web-studio (GitHub)',
+            link: 'https://github.com/MarynaShavlak/web-studio'
+        },
+        descr: [
+            'Web Studio is a professional two-page website with a responsive design for optimal viewing on any device.',
+            'It features an intuitive burger menu, modal window for smooth navigation, and a curated portfolio with overlay descriptions.',
+            'A custom checkbox enhances overall functionality.'
+        ],
+        skills: [
+            'Semantic HTML',
+            "SVG sprite usage",
+            'Flexbox Layout',
+            "Mobile-first design",
+            "Burger menu",
+            'SASS Variables',
+            'Reusable Mixins and Placeholders',
+            'Progressive enhancement',
+            "IIFE pattern",
+            "DOM manipulation",
+            "Event handling",
+            "Media queries",
+            "Accessibility (aria)",
+            "Responsive design",
+            "Code reusability",
+            'Responsive image techniques',
+        ],
+        features: [{
+            title: 'Design & Responsiveness',
+            items: [
+                'Responsive layout optimized for all screen sizes',
+                'Semantic HTML5 markup for structure and SEO',
+                'Retina-ready raster images for high-resolution displays',
+            ],
+        },
+            {
+                title: 'Navigation & Accessibility',
+                items: [
+                    'Mobile-friendly burger menu for easy navigation',
+                    'Modal window for displaying application forms',
+                    'Accessible design ensuring usability for all users',
+                    'Scroll locking via Body Scroll Lock'
+                ],
+            },
+            {
+                title: 'Portfolio & Interactivity',
+                items: [
+                    'Curated portfolio gallery showcasing diverse works',
+                    'Overlay descriptions providing project context',
+                    'Custom checkbox component for enhanced form interaction',
+                    'Subtle hover animations for engaging user experience',
+                ],
+            },
+        ],
+
+        techStack: {
+            architecture: 'Multi-page application (MPA) using Vanilla JavaScript',
+            frontend: ['HTML5', 'SCSS', 'Vanilla JavaScript', 'Vanilla JavaScript (ES6+)' ],
+            styling: [
+                'SCSS with BEM methodology',
+                'Modern Normalize CSS reset',
+            ],
+            ui: [
+                'Body Scroll Lock',
+               'Custom modal windows with JavaScript',
+
+            ],
+
 
         },
         images: [
