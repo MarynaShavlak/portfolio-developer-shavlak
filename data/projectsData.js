a01ffd145aa23078b2bb944cce2c30d9290e0de9
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-24 15:14:29 +0300

    add code fpr rock-paper-scissors

--- data/projectsData.js
+++ data/projectsData.js
@@ -1532,7 +1532,7 @@ export const projectsData = [
 
         date: 2023,
         poster: 'demo.gif',
-        timeline: '1 day',
+        timeline: '2 hours',
         website: {
             name: 'Lucky Roulette WebGame',
             link: 'https://marynashavlak.github.io/game-roulette/',
@@ -1678,6 +1678,76 @@ export const projectsData = [
             'img1.jpg',
             'img2.jpg',
             'img3.jpg',
+        ],
+    },
+    {
+        id: 'rockPaperScissors',
+        industry: 'Web Games',
+        type: 'Individual',
+        categories: ['javascript', 'html', 'css', 'webGames' ],
+        title: 'Rock Paper Scissors Game',
+        slogan: '3 hours',
+
+        date: 2023,
+        poster: 'demo.gif',
+        timeline: '2 hours',
+        website: {
+            name: 'Rock Paper Scissors Game',
+            link: 'https://marynashavlak.github.io/rock-paper-scissors/',
+        },
+        repository: {
+            name: 'rock-paper-scissors (GitHub)',
+            link: 'https://github.com/MarynaShavlak/rock-paper-scissors'
+        },
+        descr: [
+           'This is a Rock Paper Scissors game where users can select between Rock, Paper, or Scissors and play against the computer.',
+            'The game displays the player\'s and computer\'s choices, and after the round, it shows the result (win, lose, or draw) along with an animation using Lottie files.',
+            'The rules can be accessed through a modal popup.'
+        ],
+        skills: [
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "DOM Manipulation",
+            "Event Handling",
+            'Asynchronous Timing',
+            'CSS Manipulation',
+            "Function Decomposition",
+            "Game UX Logic",
+        ],
+        features: [
+            {
+                title: 'Gameplay',
+                items: [
+                    'Choose between Rock, Paper, or Scissors',
+                    'Computer makes a random choice each round',
+                    'Displays result (win, lose, or draw)',
+                    'Score tracking for each round',
+                    'Option to play again after a round is completed',
+                    'Modal for viewing game rules',
+                    'Dynamic Lottie animations for game results (e.g., win or lose)',
+                                    ]
+            },
+
+        ],
+        techStack: {
+            architecture: 'Vanilla JavaScript single-page application',
+            frontend: ['HTML5', 'CSS3', 'JavaScript (ES6+)'],
+            ui: ['Custom components',  'Lottie animations'],
+            styling: [
+                'CSS with BEM methodology',
+            ],
+
+
+        },
+        images: [
+            'demo-video.mp4',
+            'img1.jpg',
+            'img2.jpg',
+            'img3.jpg',
+            'img4.jpg',
+            'img5.jpg',
+            'img6.jpg',
         ],
     },
 ];
