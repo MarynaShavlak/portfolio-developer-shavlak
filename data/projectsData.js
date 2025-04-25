756b20f7cdd5400b04f8c93c3c150098a70d8d91
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-25 15:43:20 +0300

    add cpde for explore Kharkiv

--- data/projectsData.js
+++ data/projectsData.js
@@ -2158,7 +2158,7 @@ export const projectsData = [
             link: 'https://marynashavlak.github.io/jt-types-conversations-quiz/',
         },
         repository: {
-            name: 'jt-quiz (GitHub)',
+            name: 'js-quiz (GitHub)',
             link: 'https://github.com/MarynaShavlak/jt-types-conversations-quiz'
         },
         descr: [
@@ -2239,6 +2239,107 @@ export const projectsData = [
             styling: [
                 'CSS with BEM methodology',
                 'Modern Normalize CSS reset',
+            ],
+        },
+        images: [
+            'img1.jpg',
+            'img2.jpg',
+            'img3.jpg',
+            'img4.jpg',
+            'img5.jpg',
+            'img6.jpg',
+            'img7.jpg',
+        ],
+    },
+    {
+        id: 'exploreKharkiv',
+        industry: 'Education',
+        type: 'Individual',
+        categories: [ 'html', 'css', 'jquery' ],
+        title: 'Explore Kharkiv',
+        date: 2024,
+        poster: 'demo.gif',
+        timeline: '2 hours',
+        website: {
+            name: 'Explore Kharkiv',
+            link: 'https://marynashavlak.github.io/kharkiv-slider/',
+        },
+        repository: {
+            name: 'explore-kharkiv (GitHub)',
+            link: 'https://github.com/MarynaShavlak/kharkiv-slider'
+        },
+        descr: [
+           '"Explore Kharkiv" is an interactive web-based slider showcasing the most beautiful and iconic places in Kharkiv, Ukraine.',
+            'The project presents a visual journey through key landmarks, allowing users to explore places through images.',
+            'Each slide in the slider is paired with a tooltip that displays the name, address, and a link to view the location on Google Maps.'
+        ],
+        skills: [
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "DOM Manipulation",
+            "Event Handling",
+            'State Management',
+            'Conditional Logic & Validation',
+            'Array & Object Handling',
+            "Data-Driven Rendering",
+            'Code Splitting'
+        ],
+        features: [
+            {
+                title: 'Interactive Slider',
+                items: [
+                    'Carousel-based layout to display images of iconic Kharkiv landmarks',
+                    'Navigation through images using Previous and Next buttons',
+                    'Smooth transition effects between slides'
+                ]
+            },
+            {
+                title: 'Tooltips with Information',
+                items: [
+                    'Each image is accompanied by a tooltip with the name and address of the location',
+                    'Links provided in the tooltip to view the location on Google Maps',
+                    'Tooltip content dynamically displayed for each slide'
+                ]
+            },
+            {
+                title: 'Thumbnail Navigation',
+                items: [
+                    'Small clickable thumbnails below the main slider to navigate to specific slides',
+                    'Thumbnail images update as the user navigates through the main slider',
+                    'Hover effects highlight the active thumbnail'
+                ]
+            },
+            {
+                title: 'Responsive Design',
+                items: [
+                    'The slider adapts to different screen sizes for optimal display on both desktop and mobile devices',
+                    'Layout and controls adjust automatically to fit the screen width'
+                ]
+            },
+            {
+                title: 'Hover Effects',
+                items: [
+                    'Hover effects applied to buttons and thumbnails to provide visual feedback',
+                    'Buttons change appearance when hovered, enhancing user interaction'
+                ]
+            },
+            {
+                title: 'Smooth Navigation Controls',
+                items: [
+                    'Previous and Next buttons allow smooth navigation through the images',
+                    'Interactive navigation controls such as clickable thumbnails and hover effects'
+                ]
+            }
+        ],
+
+        techStack: {
+            architecture: 'Single-page jQuery application',
+            frontend: ['HTML5', 'CSS3', 'JavaScript (ES6+)', 'jQuery'],
+            tools: ['Prettier'],
+            ui: [ 'Google Fonts', 'Font Awesome'],
+            styling: [
+                'CSS with BEM methodology',
             ],
         },
         images: [
