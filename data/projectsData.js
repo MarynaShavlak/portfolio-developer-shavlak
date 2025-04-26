2188a155fbfea43f3cc79a820521388cddbe621e
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-04-26 19:36:16 +0300

    add code for portfolio

--- data/projectsData.js
+++ data/projectsData.js
@@ -2432,4 +2432,113 @@ export const projectsData = [
 
         ],
     },
+    {
+        id: 'carLanding',
+        slogan: '',
+        industry: 'Website',
+        type: 'Individual',
+        categories: [ 'html', 'css', 'jquery' ],
+        title: 'Car Landing Page',
+        date: 2024,
+        poster: 'car-landing.gif',
+        timeline: '4 hours',
+        website: {
+            name: 'Car Landing Page',
+            link: 'https://marynashavlak.github.io/car-landing/',
+        },
+        repository: {
+            name: 'car-landing (GitHub)',
+            link: 'https://github.com/MarynaShavlak/car-landing'
+        },
+        descr: [
+'Car Landing is a dynamic, interactive landing page designed to showcase and compare different car models' ,
+
+'The website offers a smooth user experience with dynamic model selection, real-time updates of characteristics, color choices, detailed specifications, price calculation, FAQ section, and model reviews — all without requiring a page reload.'
+        ],
+        skills: [
+            "HTML5",
+            "CSS3",
+            "JavaScript",
+            "Event Handling",
+            'State Management',
+            'Conditional Logic & Validation',
+            'Array & Object Handling',
+            "Data-Driven Rendering",
+            'Code Splitting',
+            'HTML Generation via JavaScript',
+            'Data Binding & UI Updates',
+            'CSS Animations'
+        ],
+        features: [
+            {
+                title: 'Model Selection',
+                items: [
+                    'Instant switching between Toyota Corolla and Toyota Camry models'
+                ]
+            },
+            {
+                title: 'Dynamic Car Details',
+                items: [
+                    'Display selected car’s image, specifications, and starting price',
+                    'Interactive form for selecting engine, transmission, and package options',
+                    'Automatic updates to price and specifications summary based on selections'
+                ]
+            },
+            {
+                title: 'Color Picker',
+                items: [
+                    'Choose car colors to dynamically update the displayed model image'
+                ]
+            },
+            {
+                title: 'Comparison Table',
+                items: [
+                    'Accordion-style tables for comparing different car packages'
+                ]
+            },
+            {
+                title: 'FAQ Section',
+                items: [
+                    'Accordion component featuring common customer questions'
+                ]
+            },
+            {
+                title: 'Model Reviews',
+                items: [
+                    'Interactive car review block with multiple review positions',
+                    'Display corresponding text and images for each review'
+                ]
+            },
+            {
+                title: 'Smooth Accordion Animation',
+                items: [
+                    'Smooth animations for FAQ and Variant Comparison sections'
+                ]
+            },
+            {
+                title: 'Animated Input Selection',
+                items: [
+                    'Custom animation effect for radio input selections',
+                    'Visual enhancement to improve user interaction and feedback',
+                    'Smooth animated transitions for a more engaging experience'
+                ]
+            }
+        ],
+
+        techStack: {
+            architecture: 'Single-page jQuery application',
+            frontend: ['HTML5', 'CSS3', 'JavaScript (ES6+)', 'jQuery'],
+            tools: ['Prettier'],
+            ui: [ 'Google Fonts', 'Font Awesome', 'CSS-based animations'],
+            styling: [
+                'CSS with BEM methodology',
+            ],
+        },
+        images: [
+            'car-landing.mp4',
+            'img1.jpg',
+            'img2.jpg',
+
+        ],
+    },
 ];
