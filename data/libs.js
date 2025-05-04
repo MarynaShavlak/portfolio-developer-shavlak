20e833077b9fa858decb1f8e4c9fe76b908354ac
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-04 21:59:27 +0300

    add data for my tools

--- data/libs.js
+++ data/libs.js
@@ -1,98 +1,210 @@
-export const libs = [
-    'React', 'Redux Toolkit', 'React Router',
-    'Firebase (Firestore, Auth, Cloud Functions, Storage)',
-    'TypeScript', 'Webpack', 'Vite', 'Babel', 'ESLint', 'Prettier', 'Docker',
-    'SASS/SCSS', 'Headless UI', 'React Spring', 'Use Gesture', 'TanStack Table',
-    'Jest', 'React Testing Library', 'Cypress', 'Storybook', 'Loki',
-    'Algolia', 'Monaco Editor', 'React Draft WYSIWYG', 'ApexCharts', 'Zod', 'React Device Detect',
-    'React Icons',
-    'jQuery',
-    'React Loader Spinner',
-    'React Toastify',
-    'Gulp', 'Webpack', 'Babel',
-    'PostCSS', 'Autoprefixer',
-    'Masonry Layout', 'Swiper JS', 'Font Awesome',
-    'Vanilla Tilt', 'GSAP',
-    'Body Scroll Lock', 'Modern Normalize',
-    'Axios',
-    'Redux Toolkit', 'React Redux', 'Redux Persist',
-    'React Show More Text',
-    'Formik', 'Yup',
-    'JustValidate', 'Inputmask',
-    'Material UI',
-    'Notiflix', 'Isotope', 'Owl Carousel',  'Counter-Up', 'Waypoints', 'Google Fonts', 'Font Awesome',
-    'React Icons',
-    'React Select',
-    'React Modal',
-    'React Loader Spinner',
-    'SCSS',
-    'Material UI',
-    'Material UI Icons',
-    'MUI X Date Pickers',
-    'React Icons',
-    'React Modal',
-    'React Hot Toast',
-    'Redux Async Thunk',
-    'React Avatar',
-    'Calendar Dates',
-    'Date-fns',
-    'Dayjs',
-    'React Datepicker',
-    'React Highlight Words',
-    'React Icons', 'React Loader Spinner', 'React Toastify',
-    'TypeScript',
-    'React Show More Text',
-    'React Avatar',
-    'React Star Ratings',
-    'React Circular Progress Bar',
-    'React Paginate',
-    'React Icons', 'React Loader Spinner', 'React Toastify',
-    'Nanoid', 'Prop-types',
-    'Date-fns', 'Prop-types', 'Randomcolor',
-    'Tailwind CSS',
-    'Ant Design',
-    'Vite',
-    'React Icons',
-    'React Transition Group',
-    'React Toastify',
-    'Lodash',  'UUID',
-'React Toastify',
-    'Lodash','Clsx',  'UUID',  'usehooks-ts',
-    'React Icons',
-    'React Loader Spinner',
-    'React Modal',
-    'React Select',
-    'Body Scroll Lock',
-    'Slick Carousel',
-    'BasicLightbox',
-    'Notiflix',
-    'Darkmode-js',
-    'Ultimate Pagination',
-    'Slugify', 'Nanoid', 'Randomcolor',
-    'Downshift', 'react-beautiful-dnd(Drag and Drop)',
-    'Emotion Styled (@emotion/styled)',
-    'React Avatar',
-    'React Star Ratings',
-    'React Circular Progress Bar',
-    'React Paginate',
-    'Styled Components',
-    'Modern Normalize',
-    'Date-fns', 'Prop-types', 'Randomcolor',
-    'Lottie (https://www.npmjs.com/package/@lottiefiles/lottie-player)',
-    'React Modal',
-    'React Select',
-    'Ant Design (https://ant.design/)',
-    'React Transition Group',
-    'React Toastify',
-'    just-validate (https://www.npmjs.com/package/just-validate)',
-    'Inputmask (https://www.npmjs.com/package/inputmask/v/5.0.8-beta.15)',
-    'Waypoints (https://www.npmjs.com/package/waypoints)',
-    'Counter-Up (https://www.npmjs.com/package/counterup)',
-    'Notiflix (https://www.npmjs.com/package/notiflix)',
-    'Owl Carousel 2 (https://www.npmjs.com/package/owl.carousel)',
-    'Isotope  (https://www.npmjs.com/package/isotope)'
+const toolsData = [
+    {
+        "title": "Frontend Frameworks and Libraries",
+        "items": [
+            { "name": "React", "link": "https://reactjs.org/" },
+            { "name": "React Redux", "link": "https://react-redux.js.org/" },
+            { "name": "Redux Toolkit", "link": "https://redux-toolkit.js.org/" },
+            { "name": "Redux Persist", "link": "https://github.com/rt2zz/redux-persist" },
+            { "name": "Redux Async Thunk", "link": "https://redux-toolkit.js.org/api/createAsyncThunk" },
+            { "name": "jQuery", "link": "https://jquery.com/" }
+        ]
+    },
+    {
+        "title": "State Management and Data Handling",
+        "items": [
+            { "name": "Lodash", "link": "https://lodash.com/" },
+            { "name": "Clsx", "link": "https://www.npmjs.com/package/clsx" },
+            { "name": "UUID", "link": "https://www.npmjs.com/package/uuid" },
+            { "name": "Nanoid", "link": "https://www.npmjs.com/package/nanoid" },
+            { "name": "Randomcolor", "link": "https://www.npmjs.com/package/randomcolor" },
+            { "name": "Prop-types", "link": "https://www.npmjs.com/package/prop-types" },
+
+        ]
+    },
+    {
+        "title": "Routing and Navigation",
+        "items": [
+            { "name": "React Router", "link": "https://reactrouter.com/" }
+        ]
+    },
+    {
+        "title": "Styling and CSS Frameworks",
+        "items": [
+            { "name": "SASS/SCSS", "link": "https://sass-lang.com/" },
+            { "name": "Tailwind CSS", "link": "https://tailwindcss.com/" },
+            { "name": "PostCSS", "link": "https://postcss.org/" },
+            { "name": "Autoprefixer", "link": "https://www.npmjs.com/package/autoprefixer" },
+            { "name": "Styled Components", "link": "https://styled-components.com/" },
+            { "name": "Emotion Styled", "link": "https://emotion.sh/" },
+            { "name": "Modern Normalize", "link": "https://www.npmjs.com/package/modern-normalize" }
+        ]
+    },
+    {
+        "title": "UI Component Libraries",
+        "items": [
+            { "name": "Material UI", "link": "https://mui.com/" },
+            { "name": "Material UI Icons", "link": "https://mui.com/components/material-icons/" },
+            { "name": "MUI X Date Pickers", "link": "https://mui.com/x/react-date-pickers/" },
+            { "name": "Ant Design", "link": "https://ant.design/" },
+            { "name": "Headless UI", "link": "https://headlessui.dev/" },
+            { "name": "Font Awesome", "link": "https://fontawesome.com/" },
+            { "name": "React Icons", "link": "https://www.npmjs.com/package/react-icons" }
+        ]
+    },
+    {
+        "title": "Form Handling and Validation",
+        "items": [
+            { "name": "Formik", "link": "https://formik.org/" },
+            { "name": "JustValidate", "link": "https://www.npmjs.com/package/just-validate" },
+            { "name": "Inputmask", "link": "https://www.npmjs.com/package/inputmask" },
+            { "name": "React Select", "link": "https://react-select.com/" },
+            // { "name": "Downshift", "link": "https://www.npmjs.com/package/downshift" },
+            { "name": "Zod", "link": "https://www.npmjs.com/package/zod" },
+            { "name": "Yup", "link": "https://www.npmjs.com/package/yup" }
+        ]
+    },
+    {
+        "title": "Animations and Transitions",
+        "items": [
+            { "name": "React Spring", "link": "https://react-spring.dev/" },
+            { "name": "Use Gesture", "link": "https://www.npmjs.com/package/@use-gesture/react" },
+            { "name": "React Transition Group", "link": "https://reactcommunity.org/react-transition-group/" },
+            { "name": "GSAP", "link": "https://greensock.com/gsap/" },
+            { "name": "Vanilla Tilt", "link": "https://www.npmjs.com/package/vanilla-tilt" },
+            { "name": "Lottie", "link": "https://www.npmjs.com/package/@lottiefiles/lottie-player" }
+        ]
+    },
+    {
+        "title": "Data Visualization and Tables",
+        "items": [
+            { "name": "ApexCharts", "link": "https://apexcharts.com/" },
+            { "name": "TanStack Table", "link": "https://tanstack.com/table/" },
+            { "name": "React Circular Progress Bar", "link": "https://www.npmjs.com/package/react-circular-progressbar" },
+            { "name": "React Star Ratings", "link": "https://www.npmjs.com/package/react-star-ratings" }
+        ]
+    },
+    {
+        "title": "Carousels and Layouts",
+        "items": [
+            { "name": "Swiper JS", "link": "https://swiperjs.com/" },
+            { "name": "Slick Carousel", "link": "https://www.npmjs.com/package/slick-carousel" },
+            { "name": "Owl Carousel 2", "link": "https://www.npmjs.com/package/owl.carousel" },
+            { "name": "Isotope", "link": "https://www.npmjs.com/package/isotope" },
+            { "name": "Masonry Layout", "link": "https://masonry.desandro.com/" }
+        ]
+    },
+    {
+        "title": "Modals and Notifications",
+        "items": [
+            { "name": "React Modal", "link": "https://www.npmjs.com/package/react-modal" },
+            { "name": "React Toastify", "link": "https://www.npmjs.com/package/react-toastify" },
+            { "name": "React Hot Toast", "link": "https://react-hot-toast.com/" },
+            { "name": "Notiflix", "link": "https://www.npmjs.com/package/notiflix" },
+            { "name": "BasicLightbox", "link": "https://basiclightbox.electerious.com/" }
+        ]
+    },
+    {
+        "title": "Date and Time Handling",
+        "items": [
+            { "name": "Date-fns", "link": "https://date-fns.org/" },
+            { "name": "Dayjs", "link": "https://day.js.org/" },
+            { "name": "React Datepicker", "link": "https://www.npmjs.com/package/react-datepicker" },
+            { "name": "MUI X Date Pickers", "link": "https://mui.com/x/react-date-pickers/" },
+            { "name": "Calendar Dates", "link": "https://www.npmjs.com/package/calendar-dates" }
+        ]
+    },
+    {
+        "title": "Accessibility and Device Detection",
+        "items": [
+            { "name": "Body Scroll Lock", "link": "https://www.npmjs.com/package/body-scroll-lock" },
+            { "name": "React Device Detect", "link": "https://www.npmjs.com/package/react-device-detect" }
+        ]
+    },
+    {
+        "title": "Text and Content Editing",
+        "items": [
+            { "name": "Monaco Editor", "link": "https://microsoft.github.io/monaco-editor/" },
+            { "name": "React Draft WYSIWYG", "link": "https://www.npmjs.com/package/react-draft-wysiwyg" },
+            { "name": "React Highlight Words", "link": "https://www.npmjs.com/package/react-highlight-words" }
+        ]
+    },
+    {
+        "title": "Drag and Drop",
+        "items": [
+            { "name": "react-beautiful-dnd", "link": "https://www.npmjs.com/package/react-beautiful-dnd" }
+        ]
+    },
+    {
+        "title": "Pagination and Load More",
+        "items": [
+            { "name": "React Paginate", "link": "https://www.npmjs.com/package/react-paginate" },
+            { "name": "Ultimate Pagination", "link": "https://www.npmjs.com/package/ultimate-pagination" },
+            { "name": "React Show More Text", "link": "https://www.npmjs.com/package/react-show-more-text" }
+        ]
+    },
+    {
+        "title": "Loading Indicators",
+        "items": [
+            { "name": "React Loader Spinner", "link": "https://www.npmjs.com/package/react-loader-spinner" }
+        ]
+    },
+    {
+        "title": "Search and Filtering",
+        "items": [
+            { "name": "Algolia", "link": "https://www.algolia.com/" },
+            { "name": "Slugify", "link": "https://www.npmjs.com/package/slugify" }
+        ]
+    },
+    {
+        "title": "Miscellaneous UI Enhancements",
+        "items": [
+            { "name": "React Avatar", "link": "https://www.npmjs.com/package/react-avatar" },
+            { "name": "Counter-Up", "link": "https://www.npmjs.com/package/counterup" },
+            { "name": "Waypoints", "link": "https://www.npmjs.com/package/waypoints" },
+            { "name": "Darkmode-js", "link": "https://www.npmjs.com/package/darkmode-js" }
+        ]
+    },
+    {
+        "title": "Build Tools and Development Environment",
+        "items": [
+            { "name": "Webpack", "link": "https://webpack.js.org/" },
+            { "name": "Vite", "link": "https://vitejs.dev/" },
+            { "name": "Babel", "link": "https://babeljs.io/" },
+            { "name": "Gulp", "link": "https://gulpjs.com/" },
+            { "name": "Docker", "link": "https://www.docker.com/" }
+        ]
+    },
+    {
+        "title": "Code Quality and Linting",
+        "items": [
+            { "name": "TypeScript", "link": "https://www.typescriptlang.org/" },
+            { "name": "ESLint", "link": "https://eslint.org/" },
+            { "name": "Prettier", "link": "https://prettier.io/" }
+        ]
+    },
+    {
+        "title": "Testing and Quality Assurance",
+        "items": [
+            { "name": "Jest", "link": "https://jestjs.io/" },
+            { "name": "React Testing Library", "link": "https://testing-library.com/docs/react-testing-library/intro/" },
+            { "name": "Cypress", "link": "https://www.cypress.io/" },
+            { "name": "Storybook", "link": "https://storybook.js.org/" },
+            { "name": "Loki", "link": "https://loki.js.org/" }
+        ]
+    },
+    {
+        "title": "Backend and API Integration",
+        "items": [
+            { "name": "Firebase", "link": "https://firebase.google.com/" },
+            { "name": "Axios", "link": "https://axios-http.com/" },
+            { "name": "REST Countries API", "link": "https://restcountries.com/" }
+        ]
+    },
+    {
+        "title": "Custom Hooks and Utilities",
+        "items": [
+            { "name": "usehooks-ts", "link": "https://usehooks-ts.com/" }
+        ]
+    }
 ]
-
-
-// API
-// 'https://restcountries.com/v3.1/'
