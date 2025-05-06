4a01d47a16a42e23599b1e12ef4f9099410abe1e
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-06 13:28:01 +0300

    add hover styles for card

--- js/portfolio.js
+++ js/portfolio.js
@@ -83,7 +83,7 @@ export function initPortfolio() {
 function generatePortfolioMarkUp(data) {
     return data.map((project, index) => {
         const categories = project.categories.join(' ');
-        const overview =  project.descr.join(' ').slice(0, 127) + '...'
+        const overview =  project.descr.join(' ').slice(0, 103) + '...'
         return `
         <li class="portfolio__item ${categories}" data-category="${categories}" data-id="${project.id}">
             <div class="work" data-modal="#modal_project_${index + 1}">
@@ -95,6 +95,7 @@ function generatePortfolioMarkUp(data) {
                     <p class="work__category"><span class="work__subtext">category: </span> ${project.categories.join(', ')}</p>
                     <p class="work__type"><span class="work__subtext">type: </span>${project.type}</p>
                     <p class="work__short-overview">${overview}</p>
+                    <button type="button" class="read-more-btn">READ MORE</button>
                 </div>
             </div>
         </li>`;
