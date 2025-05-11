07cd57cf0304072356d0efe9ee49576a82a665d9
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-11 14:45:24 +0300

    add code for all elements on infgraphics

--- js/cvModal/canvas/draw/lines/drawLineFigure.js
+++ js/cvModal/canvas/draw/lines/drawLineFigure.js
@@ -3,31 +3,21 @@ import { drawLineWithDash } from "./drawLineWithDash.js";
 import { drawCircleDot } from "../shapes/drawCircleDot.js";
 
 export function drawLineFigure() {
-  drawCircleDot(363, 465, 3, "#FFC2D9");
-  drawSolidLine({ x1: 363, y1: 465, x2: 310, y2: 465 }, "#FFC2D9", 3);
+  drawCircleDot(363, 545, 3, "#FFC2D9");
+  drawSolidLine({ x1: 363, y1: 545, x2: 240, y2: 545 }, "#FFC2D9", 3);
   drawLineWithDash(
-    { x1: 300, y1: 465, x2: 260, y2: 465 },
+    { x1: 230, y1: 545, x2: 200, y2: 545 },
     [3, 4],
     "#FFC2D9",
     3,
   );
   drawLineWithDash(
-    { x1: 255, y1: 465, x2: 210, y2: 495 },
+    { x1: 195, y1: 545, x2: 115, y2: 575 },
     [3, 4],
     "#FFC2D9",
     3,
   );
-  drawLineWithDash(
-    { x1: 205, y1: 495, x2: 120, y2: 495 },
-    [3, 4],
-    "#FFC2D9",
-    3,
-  );
-  drawLineWithDash(
-    { x1: 115, y1: 495, x2: 110, y2: 495 },
-    [2, 2],
-    "#FFC2D9",
-    15,
-  );
-  drawSolidLine({ x1: 108, y1: 495, x2: 0, y2: 495 }, "#FFC2D9", 3);
+  drawLineWithDash({ x1: 110, y1: 575, x2: 70, y2: 575 }, [3, 4], "#FFC2D9", 3);
+  drawLineWithDash({ x1: 70, y1: 575, x2: 65, y2: 575 }, [2, 2], "#FFC2D9", 15);
+  drawSolidLine({ x1: 60, y1: 575, x2: 0, y2: 575 }, "#FFC2D9", 3);
 }
