91f8ab1edfabaaca081ecef1262e5af0fbd69ac5
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-05-14 17:05:13 +0300

    add code for partials

--- index.js
+++ index.js
@@ -14,52 +14,52 @@ import { initSoftSkillsSection } from "./js/softSkills/initSoftSkillsSection.js"
 $(document).ready(async function () {
   try {
     // header and hero
-    await insertPartial("#header", "../partials/header.html");
-    await insertPartial("#main", "../partials/hero.html");
+    await insertPartial("#header", "./partials/header.html");
+    await insertPartial("#main", "./partials/hero.html");
     initHeroSection();
 
     // about
-    await insertPartial("#main", "../partials/about.html");
+    await insertPartial("#main", "./partials/about.html");
     // soft skills
-    await insertPartial("#main", "../partials/softSkills.html");
+    await insertPartial("#main", "./partials/softSkills.html");
     initSoftSkillsSection();
     // contacts
-    await insertPartial("#main", "../partials/contacts.html");
+    await insertPartial("#main", "./partials/contacts.html");
     initContactsSection();
 
     // tools
-    await insertPartial("#main", "../partials/tools.html");
+    await insertPartial("#main", "./partials/tools.html");
     initToolsSection();
 
     //education
-    await insertPartial("#main", "../partials/education.html");
+    await insertPartial("#main", "./partials/education.html");
     initEducationSection();
 
     //work experience
-    await insertPartial("#main", "../partials/workExperience.html");
+    await insertPartial("#main", "./partials/workExperience.html");
     initWorkSection();
 
     //portfolio
-    await insertPartial("#main", "../partials/portfolio.html");
+    await insertPartial("#main", "./partials/portfolio.html");
     initPortfolio();
 
     // testimonials
-    await insertPartial("#main", "../partials/reviews.html");
+    await insertPartial("#main", "./partials/reviews.html");
     initReviewsSlider();
 
     // blog
-    await insertPartial("#main", "../partials/blog.html");
+    await insertPartial("#main", "./partials/blog.html");
     initBlog();
 
     // footer
-    await insertPartial("#footer", "../partials/footer.html");
+    await insertPartial("#footer", "./partials/footer.html");
 
     //hire modal
-    await insertPartial("#hire-modal", "../partials/hireModal.html");
+    await insertPartial("#hire-modal", "./partials/hireModal.html");
     initHireModal();
 
     //cv modal
-    await insertPartial(".cv-modal", "../partials/cvModal.html");
+    await insertPartial(".cv-modal", "./partials/cvModal.html");
     initCVModal();
   } catch (err) {
     console.error("Error loading one or more partials:", err);
