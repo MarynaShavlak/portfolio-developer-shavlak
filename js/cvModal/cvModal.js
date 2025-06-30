9e3bad58330f08e9e881a2bd01f596234cf900f9
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-06-30 15:31:03 +0300

    fix mobile styles bugs

--- js/cvModal/cvModal.js
+++ js/cvModal/cvModal.js
@@ -1,18 +1,12 @@
 import { downloadPDF } from "../utils/downLoadPDF.js";
 import { drawFigure } from "./canvas/canvas.js";
+import { downloadCV } from "../downloadCV/downloadCV.js";
 
 export function initCVModal() {
   $('[data-modal="cv-modal"]').on("click", showCVModal);
   $(".close-modal-btn").on("click", hideCVModal);
   drawFigure();
   downloadCV();
-}
-
-function downloadCV() {
-  $(".download-cv-btn").on("click", function () {
-    const pdfPath = "./Shavlak_Maryna_Frontend_Developer__CV.pdf";
-    downloadPDF(pdfPath, "Shavlak_Maryna_Frontend_Developer__CV.pdf");
-  });
 }
 
 function showCVModal() {
---
+++ js/downloadCV/downloadCV.js
@@ -0,0 +1,8 @@
+import { downloadPDF } from "../utils/downLoadPDF.js";
+
+export function downloadCV() {
+  $(".download-btn").on("click", function () {
+    const pdfPath = "./Shavlak_Maryna_Frontend_Developer__CV.pdf";
+    downloadPDF(pdfPath, "Shavlak_Maryna_Frontend_Developer__CV.pdf");
+  });
+}
