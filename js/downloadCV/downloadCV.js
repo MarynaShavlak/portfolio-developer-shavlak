b4309ed9871baa1c4b1b2b56aeba68a2c53d1d7c
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-07-18 13:57:53 +0300

    add changes for CV info

--- js/downloadCV/downloadCV.js
+++ js/downloadCV/downloadCV.js
@@ -2,7 +2,7 @@ import { downloadPDF } from "../utils/downLoadPDF.js";
 
 export function downloadCV() {
   $(".download-btn").on("click", function () {
-    const pdfPath = "./Shavlak_Maryna_Frontend_Developer__CV.pdf";
-    downloadPDF(pdfPath, "Shavlak_Maryna_Frontend_Developer__CV.pdf");
+    const pdfPath = "./Shavlak_Maryna_Frontend_Developer_CV.pdf";
+    downloadPDF(pdfPath, "Shavlak_Maryna_Frontend_Developer_CV.pdf");
   });
 }
