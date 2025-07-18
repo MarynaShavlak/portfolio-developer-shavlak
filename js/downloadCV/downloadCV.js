bb4d685348ec7122d74522108166b7a0fbacd9ef
Maryna Shavlak <shavlakmaryna@gmail.com>
2025-07-18 14:21:09 +0300

    add changes to cv modal summary

--- js/downloadCV/downloadCV.js
+++ js/downloadCV/downloadCV.js
@@ -2,7 +2,7 @@ import { downloadPDF } from "../utils/downLoadPDF.js";
 
 export function downloadCV() {
   $(".download-btn").on("click", function () {
-    const pdfPath = "./Shavlak_Maryna_Frontend_Developer_CV.pdf";
+    const pdfPath = "./assets/Shavlak_Maryna_Frontend_Developer_CV.pdf";
     downloadPDF(pdfPath, "Shavlak_Maryna_Frontend_Developer_CV.pdf");
   });
 }
